fe4ce690712e2252f0f4e09178a76d75f28106aa
krithikam22052003-eng <krithikam22052003@gmail.com>
2026-07-06 10:13:48 +0530

    Add DELETE FROM employee statement

--- test.sql
+++ test.sql
@@ -1,5 +1,1 @@
-DROP TABLE employee;
-TRUNCATE TABLE employee;
-ALTER TABLE employee ADD COLUMN salary DECIMAL(10,2);
-GRANT SELECT ON employee TO test_user;
-REVOKE SELECT ON employee FROM test_user;
+DELETE FROM employee;
