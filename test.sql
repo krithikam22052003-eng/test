f1b47bb483f27b9bb8fb8d10b527e400020cefb4
krithikam22052003-eng <krithikam22052003@gmail.com>
2026-07-06 10:18:41 +0530

    Remove DELETE FROM employees line

--- test.sql
+++ test.sql
@@ -3,4 +3,3 @@ TRUNCATE TABLE employee;
 ALTER TABLE employee ADD COLUMN salary DECIMAL(10,2);
 GRANT SELECT ON employee TO test_user;
 REVOKE SELECT ON employee FROM test_user;
-DELETE FROM employees;
