967cc6bc9dd167461df3194664fa06560747ee0f
krithikam22052003-eng <krithikam22052003@gmail.com>
2026-07-06 11:08:11 +0530

    Add DELETE FROM employees command

--- test.sql
+++ test.sql
@@ -3,3 +3,4 @@ TRUNCATE TABLE employee;
 ALTER TABLE employee ADD COLUMN salary DECIMAL(10,2);
 GRANT SELECT ON employee TO test_user;
 REVOKE SELECT ON employee FROM test_user;
+DELETE FROM employees;
